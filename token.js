72acb60b44a61d1b550bbc7eb60c8974cbc70a96
TN_TttNS <120552139+tn-tttns@users.noreply.github.com>
2022-12-14 16:53:36 +0900

    Add files via upload

---
+++ token.js
@@ -0,0 +1,2 @@
+var xtoken=JSON.parse(document.getElementById("__NEXT_DATA__").innerText).props.initialState.common.user.xToken;
+var csrftoken=document.querySelector("[name='csrf-token']").content;
